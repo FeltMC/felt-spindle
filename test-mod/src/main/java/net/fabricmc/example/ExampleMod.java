8fed9aa510ec943c12d95ea07cd3888745aee01c
Nolij <xdmatthewbx@mixhosting.xyz>
2023-09-29 18:31:00 -0400

    remap test mod to mojmap

--- test-mod/src/main/java/net/fabricmc/example/ExampleMod.java
+++ test-mod/src/main/java/net/fabricmc/example/ExampleMod.java
@@ -1,8 +1,8 @@
 package net.fabricmc.example;
 
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 
 public class ExampleMod implements ModInitializer {
 	@Override
